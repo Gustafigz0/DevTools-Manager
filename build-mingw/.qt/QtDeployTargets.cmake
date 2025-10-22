7a5a4ce00529c8a53a03b954a1cd4c23d2074432
Guts <gustafigsouza@gmail.com>
2025-10-22 23:35:59 +0100

    SimplesGUI

--- build-mingw/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_minimal_FILE C:/Users/Guts/Documents/GitHub/Gui/build-mingw/minimal.exe)
-set(__QT_DEPLOY_TARGET_minimal_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_minimal_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
--- scr/MainWindow.cpp
+++ scr/MainWindow.cpp
@@ -14,78 +14,27 @@ void MainWindow::setupUi() {
     root->setContentsMargins(16,16,16,16);
     root->setSpacing(24);
 
-    // Label lateral de fundo
     sidebarBox_ = new QWidget;
     sidebarBox_->setStyleSheet(R"(
         background: #232323;
         border-radius: 24px;
         min-width: 220px;
         max-width: 240px;
+        margin-top: 8px;
+        padding: 36px 24px;
     )");
 
-    // Label topo área usuário/configs
-    userInfoLabel_ = new QWidget;
-    userInfoLabel_->setStyleSheet(R"(
-        background: #222;
-        border-radius: 18px;
-        padding: 24px 32px;
-        margin-bottom:18px;
-        box-shadow: 0px 0px 8px #0001;
-    )");
-    auto* userLayout = new QHBoxLayout(userInfoLabel_);
-    QLabel* userLabel = new QLabel("Usuário: Admin");
-    userLabel->setStyleSheet("color: #5a6269ff; font-size: 17px; font-weight: 600;");
-    userLayout->addWidget(userLabel);
-
-    // Label de fundo dos produtos
     productsBg_ = new QWidget;
     productsBg_->setStyleSheet(R"(
         background: #242526;
         border-radius: 28px;
         padding: 36px 24px;
-        margin-top:8px;
-        box-shadow: 0px 4px 24px #0002;
+        margin-top: 8px;
+        box-shadow: 0px 4px 24px #990c0c22;
     )");
     productsLayout_ = new QVBoxLayout(productsBg_);
     productsLayout_->setSpacing(18);
     productsLayout_->setContentsMargins(10,10,10,18);
-
-    // Adiciona área topo info usuário
-    productsLayout_->addWidget(userInfoLabel_);
-
-    // Adiciona apenas UM produto/label
-    QWidget* itemCard = new QWidget;
-    itemCard->setStyleSheet(R"(
-        background: #232323;
-        border-radius:18px;
-        padding:22px 24px;
-        margin-bottom:10px;
-        box-shadow: 0 2px 9px #0001;
-    )");
-    auto* cardLayout = new QHBoxLayout(itemCard);
-    QLabel* icon = new QLabel;
-    icon->setFixedSize(44,44);
-    icon->setStyleSheet("background:#111;border-radius:12px;");
-    QLabel* name = new QLabel("<b>Produto Único</b>");
-    name->setStyleSheet("color:#fff;font-size:17px;");
-    QLabel* cat = new QLabel("Categoria");
-    cat->setStyleSheet("color:#ccc;font-size:15px;");
-    QLabel* id = new QLabel("ID123");
-    id->setStyleSheet("color:#58abfa;");
-    QLabel* price = new QLabel("$00,00");
-    price->setStyleSheet("color:#fff;");
-    QLabel* qty = new QLabel("Qtd: 0");
-    qty->setStyleSheet("color:#fff;");
-    cardLayout->addWidget(icon);
-    cardLayout->addWidget(name);
-    cardLayout->addWidget(cat);
-    cardLayout->addWidget(id);
-    cardLayout->addWidget(price);
-    cardLayout->addWidget(qty);
-    cardLayout->addStretch();
-    productsLayout_->addWidget(itemCard);
-
-    productsLayout_->addStretch();
 
     // Adiciona as labels de fundo (sidebar + produtos)
     root->addWidget(sidebarBox_,0);
